42a45b689729cbf9d564cd6929b30844f9fd147d
TursunovFirdavs <tursunovfirdavs571@gmail.com>
2024-05-05 23:05:13 +0500

    categories render

--- src/components/catecory/Category.tsx
+++ src/components/catecory/Category.tsx
@@ -4,24 +4,22 @@ import { useGetCategories } from '@/providers/query/useGetCategories';
 import React from 'react'
 import { TfiMenuAlt } from "react-icons/tfi";
 
-const data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-
 export default function Category() {
 
-    const { data: category } = useGetCategories()
-    console.log(category);
+    const { data, isLoading } = useGetCategories()
+    console.log(data);
     
 
-    return (
+    return isLoading ? <div className='w-[275px] h-[525px] flex items-center justify-center'><div className="lds-ripple"><div></div><div></div></div></div> : (
         <div className='max-w-[275px] w-full bg-white px-6 py-4 rounded-sm overflow-hidden'>
             <div className='flex items-center gap-4 pb-4'>
                 <TfiMenuAlt className='text-xl' />
                 <p className='text-[14px] font-semibold'>SHOP BY CATEGORY</p>
             </div>
-            {data?.map((item: number) => (
-                <div key={item} className='border-t py-2 flex items-center gap-3'>
-                    <img className='w-7' src="https://shop.theclub.com.hk/media/catalog/product/cache/328772890ce7c3d4dd74ab840a380f82/r/e/redminote12probl_1.png" alt="" />
-                    <p>Category {item}</p>
+            {data?.results.map((item: any) => (
+                <div key={item.id} className='border-t py-2 flex items-center gap-3'>
+                    <img className='w-7' src={item.image} alt="" />
+                    <p>{item.title}</p>
                 </div>
             ))}
         </div>
