4d1dbf6eb4da58116ed3e12d21db8de384e0eeb2
TursunovFirdavs <tursunovfirdavs571@gmail.com>
2024-05-21 21:05:22 +0500

    like product in single product

--- src/app/product/components/Single-product.tsx
+++ src/app/product/components/Single-product.tsx
@@ -4,8 +4,10 @@ import { product_images } from '@/types';
 import React, { useState } from 'react'
 import { FiStar } from 'react-icons/fi';
 import { FaRegStar } from "react-icons/fa";
-import { FaFacebookF, FaTwitter, FaPinterest, FaLinkedinIn} from "react-icons/fa";
+import { FaFacebookF, FaTwitter, FaPinterest, FaLinkedinIn } from "react-icons/fa";
 import { IoLogoGoogleplus } from "react-icons/io";
+import { useDispatch, useSelector } from 'react-redux';
+import { dislike, liked } from '@/redux/reducer/like';
 
 
 
@@ -13,6 +15,11 @@ import { IoLogoGoogleplus } from "react-icons/io";
 const SingleProduct = ({ id }: any) => {
     const [select, setSelect] = useState(0)
     const { data } = useGetSingleVariant(id)
+    const dispatch = useDispatch()
+    const { like } = useSelector((state: any) => state.like)
+    console.log(data);
+
+
 
 
 
@@ -23,7 +30,7 @@ const SingleProduct = ({ id }: any) => {
                 <div className='flex flex-col items-center'>
                     <img className='max-w-[714px] w-full max-h-[470px] h-full object-contain' src={data?.images[select].image} alt="" />
                     <div className='flex gap-5 mt-4'>
-                        {data?.images?.slice(0,2).map((item: product_images, i: number) => (
+                        {data?.images?.slice(0, 2).map((item: product_images, i: number) => (
                             <img onClick={() => setSelect(i)} className='md:w-[163px] w-[140px] object-cover' src={item.image} alt="" />
                         ))}
                     </div>
@@ -37,27 +44,32 @@ const SingleProduct = ({ id }: any) => {
                     <p className='md:text-[28px] text-[20px] font-bold'>{data?.price}</p>
                     <p className='max-w-[700px] my-4'>{data?.other_detail?.replace(/<[^>]+>/g, "")} Lorem ipsum dolor sit, amet consectetur adipisicing elit. Architecto beatae labore error est officiis cumque incidunt pariatur dolor animi voluptates odit, quisquam ab ullam praesentium possimus alias obcaecati doloribus aperiam! Lorem, ipsum dolor sit amet consectetur adipisicing elit. Impedit quidem expedita amet, aut id soluta laudantium similique facilis temporibus ex.</p>
                     <button className='bg-secondary-yellow py-1.5 px-10 rounded-3xl text-[14px] font-semibold'>+ Add To Cart</button>
-                    <div className='flex items-center wishlist gap-2 mt-5 cursor-pointer w-[150px]'>
-                        <FaRegStar className='text-secondary-dark text-xl icon'/>
-                        <p className='text-[14px] w-text'>Add To Wishlist</p>
-                    </div>
+                    {like?.findIndex((liked: any) => liked.id === data?.id) !== -1 ?
+                        <div onClick={() => dispatch(dislike(data))} className={`flex items-center wishlist gap-2 mt-5 text-secondary-yellow cursor-pointer w-[150px]`}>
+                            <FaRegStar className='text-secondary-dark text-xl icon' />
+                            <p className='text-[14px] w-text'>Add To Wishlist</p>
+                        </div> :
+                        <div onClick={() => dispatch(liked(data))} className={`flex items-center wishlist gap-2 mt-5 cursor-pointer w-[150px]`}>
+                            <FaRegStar className='text-secondary-dark text-xl icon' />
+                            <p className='text-[14px] w-text'>Add To Wishlist</p>
+                        </div>}
                     <div className='flex items-center md:gap-6 gap-5 mt-5 mb-[50px]'>
                         <p>Share This Product</p>
                         <div className='flex items-center md:gap-4 gap-2'>
                             <div className='w-10 h-10 rounded-full flex items-center justify-center text-secondary-dark bg-[#F1F1F1]'>
-                            <FaFacebookF/>
+                                <FaFacebookF />
                             </div>
                             <div className='w-10 h-10 rounded-full flex items-center justify-center text-secondary-dark bg-[#F1F1F1]'>
-                            <FaTwitter/>
+                                <FaTwitter />
                             </div>
                             <div className='w-10 h-10 rounded-full flex items-center justify-center text-secondary-dark bg-[#F1F1F1]'>
-                            <FaPinterest/>
+                                <FaPinterest />
                             </div>
                             <div className='w-10 h-10 rounded-full flex items-center justify-center text-secondary-dark bg-[#F1F1F1]'>
-                            <IoLogoGoogleplus/>
+                                <IoLogoGoogleplus />
                             </div>
                             <div className='w-10 h-10 rounded-full flex items-center justify-center text-secondary-dark bg-[#F1F1F1]'>
-                            <FaLinkedinIn/>
+                                <FaLinkedinIn />
                             </div>
                         </div>
                     </div>
