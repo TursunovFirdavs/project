66814dc4aae375c226595d7b7f221322ab46e0cc
TursunovFirdavs <tursunovfirdavs571@gmail.com>
2024-05-21 14:42:29 +0500

    sub category media

--- src/app/sub/[id]/page.tsx
+++ src/app/sub/[id]/page.tsx
@@ -2,15 +2,17 @@ import React from 'react'
 import SubProducts from '../components/sub-products'
 import Categories from '@/components/catecory/Categories';
 
-const Sub = ({params} : any) => {
-    console.log(params);
-    
+const Sub = ({ params }: any) => {
+  console.log(params);
+
   return (
     <div className='bg-secondary-light'>
-        <div className='container py-8 flex gap-10'>
-            <Categories/>
-        <SubProducts id={params?.id}/>
+      <div className='container py-8 flex gap-10'>
+        <div className='xl:block hidden'>
+          <Categories />
         </div>
+        <SubProducts id={params?.id} />
+      </div>
     </div>
   )
 }
